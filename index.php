11d69be9702602e634d15df83148d491544760ec
mmmenno <menno@denengelse.nl>
2019-11-19 15:20:56 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+
+	<title>Rotterdams Publiek</title>
+	<meta name="description" content="Rotterdams Publiek - culturele geschiedenissen van Rotterdammers">
+	<meta name="author" content="Islands of Meaning">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/styles.css">
+	
+</head>
+
+<body>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md abt-locations">
+				<h2>Plaatsen van vertier</h2>
+				<p>
+					Bioscopen, theaters, concertzalen, clubs en musea - waar staan en stonden ze. Je kan het (straks) allemaal bekijken <a href="">op de locatieskaart</a>.
+				</p>
+			</div>
+			<div class="col-md quote">
+				<blockquote>&ldquo;Quote uit interview&bdquo;</blockquote>
+			</div>
+			<div class="col-md abt-interviews">
+				<h2>Interviews</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md abt-actors">
+				<h2>Wie is wie?</h2>
+				<p>
+					Ray Charles in De Doelen, Jayne Mansfield op de middenstip van het Kasteel en Salvador Dali in Museum Boijmans. Wie drukte zijn of haar stempel op het culturele landschap? Of was toevallig in de buurt? Check onze <a href="">wie-is-wie pagina</a>.
+				</p>
+			</div>
+			<div class="col-md abt-events">
+				<h2>Waargebeurd</h2>
+				<p>
+					Premières, festivals, koninklijk bezoek - op beeld vastgelegde gebeurtenissen uit het culturele leven van de stad tonen we in <a href="">ons gebeurtenissenoverzicht</a>.
+				</p>
+			</div>
+			<div class="col-md quote">
+				<blockquote>&ldquo;Quote uit interview&bdquo;</blockquote>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md quote">
+				<blockquote>&ldquo;Quote uit interview&bdquo;</blockquote>
+			</div>
+			<div class="col-md abt-timemachine">
+				<h2>Tijdmachine</h2>
+				<p>Teleporteer jezelf naar R'dam in het jaar...</p>
+				<form id="timemachineform">
+					<select>
+						<option>1</option>
+						<option>2</option>
+					</select>
+					<select>
+						<option>9</option>
+						<option>0</option>
+					</select>
+					<select>
+						<option>0</option>
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option selected="selected">8</option>
+						<option>9</option>
+					</select>
+					<select>
+						<option>0</option>
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option selected="selected">4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+					</select>
+
+					<button>🚀</button>
+				</form>
+				<p>... en bekijk het culturele landschap van dat moment.</p>
+			</div>
+			<div class="col-md abt-us">
+				<h2>Rotterdams Publiek</h2>
+
+				<p>
+					Binnen dit project proberen we het rijke uitgaansverleden van Rotterdam met behulp van interviews en beeldmateriaal uit het Stadsarchief weer tot leven te wekken.
+				</p>
+
+				<p>
+					We doen dit vooral door <a href="">data</a> te verbeteren en verbinden.
+				</p>
+
+				<p>
+					Dit project is mede mogelijk gemaakt door het Stimuleringsfonds.
+				</p>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
