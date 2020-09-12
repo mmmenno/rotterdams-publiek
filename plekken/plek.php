f4006920b7a2ef2040f32f43c45af626e77f47a4
Menno den Engelse <menno@denengelse.nl>
2020-09-12 19:50:39 +0200

    debug

--- plekken/plek.php
+++ plekken/plek.php
@@ -1,6 +1,74 @@
 <?php
 
+if(!isset($_GET['qid'])){
+  $qid = "Q179426";
+}else{
+  $qid = $_GET['qid'];
+}
 
+include("functions.php");
+
+$sparql = "
+SELECT ?item ?itemLabel ?typeLabel ?bouwjaar ?sloopjaar ?iseentypeLabel ?starttype ?eindtype ?naamstring ?startnaam ?eindnaam ?image ?straatLabel ?coords ?bagid ?sitelink ?next ?nextLabel ?prev ?prevLabel WHERE {
+  
+  VALUES ?item { wd:" . $qid . " }
+  ?item wdt:P31 ?type .
+  OPTIONAL{
+	 ?item wdt:P625 ?coords .
+  }
+  OPTIONAL{
+		?item wdt:P18 ?image .
+	 }
+  OPTIONAL{
+		?item wdt:P571 ?bouwjaar .
+	 }
+  OPTIONAL{
+		?item wdt:P576 ?sloopjaar .
+	 }
+  OPTIONAL{
+		?item p:P31 ?iseen .
+		?iseen ps:P31 ?iseentype .
+		?iseen pq:P580 ?starttype .
+		?iseen pq:P582 ?eindtype .
+	 }
+  OPTIONAL{
+		?item p:P31 ?iseen .
+		?iseen ps:P31 ?iseentype .
+		?iseen pq:P580 ?starttype .
+		?iseen pq:P582 ?eindtype .
+	 }
+  OPTIONAL{
+		?item p:P2561 ?naam .
+		?naam ps:P2561 ?naamstring .
+		?naam pq:P580 ?startnaam .
+		?naam pq:P582 ?eindnaam .
+	 }
+  OPTIONAL{
+    ?item wdt:P1398 ?prev .
+  }
+  OPTIONAL{
+    ?item wdt:P167 ?next .
+  }
+  OPTIONAL{
+    ?item wdt:P669 ?straat .
+  }
+  OPTIONAL{
+  	?sitelink schema:about ?item;
+    schema:isPartOf <https://nl.wikipedia.org/>;
+  }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language \"nl,en\". }
+}
+ORDER BY ?typeLabel ?itemLabel
+LIMIT 1000";
+
+
+$endpoint = 'https://query.wikidata.org/sparql';
+
+$json = getSparqlResults($endpoint,$sparql);
+$data = json_decode($json,true);
+
+
+print_r($data);
 
 echo "hi";
 
